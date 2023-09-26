00cf95f504487324c3d61ed1b1aec1c8e597419d
Masaki Mita <mimimita3131@gmail.com>
2023-09-26 22:39:53 +0900

    Update repaint.js

--- others/repaint.js
+++ others/repaint.js
@@ -4,7 +4,7 @@ const {createPaint} = require("../others/createPaint");
 module.exports = {
     async execute_repaint(interaction) {
         if (!interaction.isButton()) return;
-        if (interaction.commandName !== 'repaint') return;
+        if (interaction.customId !== 'repaint') return;
 
         await interaction.deferReply();
         const message = await interaction.channel.messages.fetch(interaction.message.reference.messageId);
@@ -13,8 +13,15 @@ module.exports = {
         try {
             const result = await createPaint(prompt, message.author.id);
             const file = new AttachmentBuilder(result, {name: "result.png", description: prompt});
+            const row = new ActionRowBuilder()
+                .addComponents(
+                    new ButtonBuilder()
+                        .setCustomId('repaint')
+                        .setLabel('再生成')
+                        .setStyle(ButtonStyle.Primary),
+                );
             await interaction.deleteReply();
-            await message.reply({files: [file]});
+            await message.reply({files: [file], components: [row]});
         } catch (err) {
             console.log(err);
             await interaction.editReply("```diff\n-何らかの問題が発生しました。\n```");
